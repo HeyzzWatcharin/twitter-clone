0bf0d1a9d0c8c60f166131115fec0de944e43359
HeyzzWatcharin <watcharinrachadet123@gmail.com>
2023-06-06 15:30:34 +0700

    initial project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div class="bg-red-500">Hello World</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
